10597d23967c0887d2b328abe261261957c48f86
CB <brandon.cozart2000@gmail.com>
2022-07-07 19:28:50 -0400

    first

---
+++ app/src/main/java/com/example/shoebill/LoginScreen.java
@@ -0,0 +1,57 @@
+package com.example.shoebill;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginScreen#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginScreen extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+
+    public LoginScreen() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static LoginScreen newInstance() {
+        LoginScreen fragment = new LoginScreen();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login_screen, container, false);
+
+
+
+        return view;
+    }
+}
